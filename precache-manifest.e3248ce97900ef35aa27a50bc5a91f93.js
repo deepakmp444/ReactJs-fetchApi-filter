c15ccd216f2d8fe275b543702079813550952c53
deepakmp444 <deepakmp444@gmail.com>
2020-08-03 00:46:57 +0530

    Updates

---
+++ precache-manifest.e3248ce97900ef35aa27a50bc5a91f93.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91631b90229e78ef43f2bf4aae137233",
+    "url": "/ReactJs-fetchApi-filter/index.html"
+  },
+  {
+    "revision": "277c75b104a7de33a4e7",
+    "url": "/ReactJs-fetchApi-filter/static/css/main.27df069b.chunk.css"
+  },
+  {
+    "revision": "1bb5dd2a6657ff2ee685",
+    "url": "/ReactJs-fetchApi-filter/static/js/2.b6f2604e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactJs-fetchApi-filter/static/js/2.b6f2604e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "277c75b104a7de33a4e7",
+    "url": "/ReactJs-fetchApi-filter/static/js/main.e0fac62b.chunk.js"
+  },
+  {
+    "revision": "d86df33c23c3e174ae5c",
+    "url": "/ReactJs-fetchApi-filter/static/js/runtime-main.e81ee7d0.js"
+  }
+]);
